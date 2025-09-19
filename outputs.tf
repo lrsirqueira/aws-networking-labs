a138616f354429795708cc2eaae17062c15069ef
lrsirqueira <67562978+lrsirqueira@users.noreply.github.com>
2025-09-18 21:53:38 -0300

    Iniciar lab vpc-basico

--- outputs.tf
+++ outputs.tf
@@ -11,8 +11,3 @@ output "vpc_cidr" {
   description = "CIDR block da VPC"
   value       = aws_vpc.my-first-vpc.cidr_block
 }
-
-output "availability_zone" {
-  description = "Zona de disponibilidade utilizada"
-  value       = aws_subnet.public_subnet.availability_zone
-}
