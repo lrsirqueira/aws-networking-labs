bda8410eb264107c166a154d48d85fe3334e8b6f
lrsirqueira <67562978+lrsirqueira@users.noreply.github.com>
2025-09-19 18:14:03 -0300

    lab 2

--- route-table.tf
+++ route-table.tf
@@ -2,16 +2,6 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-
-  route {
-    ipv6_cidr_block                = "::/0"
-    egress_only_gateway_id         = aws_egress_only_internet_gateway.main.id
-  }
-
   tags = {
     Name = "my-firtst-private-route-table"  # Mantendo o typo original
     lab  = "lab"
