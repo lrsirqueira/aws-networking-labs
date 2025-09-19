cd8a494f3d76607b2e10e8def2fcaf526394b7bb
lrsirqueira <67562978+lrsirqueira@users.noreply.github.com>
2025-09-19 14:16:16 -0300

    ajustando a config padrao e terraform state

--- main.tf
+++ main.tf
@@ -1,19 +1,47 @@
 # Configure the AWS Provider
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  required_version = ">= 1.0"
+
+  # Backend S3 para armazenar o state
+  backend "s3" {
+    bucket         = "aws-networking-labs-terraform-state-lrsirqueira"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks-aws-networking-labs"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
 
-# Data source for availability zones
+# *********************************************************************************************#
+# ******************************* Locals (Tags Comuns) ****************************************#
+# *********************************************************************************************#
+locals {
+  common_tags = {
+    Environment = "Lab"
+    Owner       = "Luis"
+    Project     = "AWS-Networking-Specialty"
+    ManagedBy   = "Terraform"
+    lab         = ""lab
+  }
+}
+
+
+# *********************************************************************************************#
+# ******************************* Data Sources *************************************************#
+# *********************************************************************************************#
+
+# Obter zonas de disponibilidade
 data "aws_availability_zones" "available" {
   state = "available"
 }
